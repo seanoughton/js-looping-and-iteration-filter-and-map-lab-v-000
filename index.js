2d981fa33d89595294b8d61021f017d3eb3a5530
Sean Oughton <seanoughton@gmail.com>
2018-06-22 16:17:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 function driversWithRevenueOver(drivers,revenue) {
   return drivers.filter(function(driver){ return driver.revenue > revenue;});
 }
+
+function driverNamesWithRevenueOver(drivers,revenue) {
+  return drivers.filter(function(driver){ return driver.revenue > revenue;});
+}
